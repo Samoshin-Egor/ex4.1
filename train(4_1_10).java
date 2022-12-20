0a23f7fe221f94a021557ca47fc66615e7d1a96a
Samoshin-Egor <121015765+Samoshin-Egor@users.noreply.github.com>
2022-12-20 10:01:02 +0300

    train(4_1_10).java

---
+++ train(4_1_10).java
@@ -0,0 +1,42 @@
+package ex4_1_10;
+
+public class train extends transport{
+    public double getTimeP() {
+        return timeP;
+    }
+
+
+    public double getPriceP() {
+        return priceP;
+    }
+
+
+    public double getTimeG() {
+        return timeG;
+    }
+
+
+    public double getPriceG() {
+        return priceG;
+    }
+
+
+    public void setTimeP(double timeP) {
+        this.timeP=timeP;
+    }
+
+
+    public void setPriceP(double priceP) {
+        this.priceP=priceP;
+    }
+
+
+    public void setTimeG(double timeG) {
+        this.timeG=timeG;
+    }
+
+
+    public void setPriceG(double priceG) {
+        this.priceG=priceG;
+    }
+}
