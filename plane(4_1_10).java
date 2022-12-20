f37899ffd2890346a83c7696772ca7f814e84a47
Samoshin-Egor <121015765+Samoshin-Egor@users.noreply.github.com>
2022-12-20 09:59:21 +0300

    plane(4_1_10).java

---
+++ plane(4_1_10).java
@@ -0,0 +1,16 @@
+package ex4_1_10;
+
+public class plane extends transport{
+    public double getTimeP() {
+        return timeP;
+    }
+
+
+    public double getPriceP() {
+        return priceP;
+    }
+
+
+    public double getTimeG() {
+        return timeG;
+    }
